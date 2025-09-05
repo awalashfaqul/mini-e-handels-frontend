6cb0d3ce77d79d339fe6332b4e2acada55b4a566
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2025-09-05 20:37:04 +0200

    Header updated

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ export default function App() {
     <div className="min-h-screen flex flex-col">
       <header className="bg-blue-600 text-white p-4">
         <h1 className="text-xl font-bold">Mini E-Commerce Store</h1>
-        <h4 className="text-xl font-bold">Optimizely Inspired</h4>
       </header>
       <Navbar />
       <main className="flex-1 p-6">
