260bdd1bfc38a461443a2bf69047497f658e8b5f
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2025-09-05 13:37:16 +0200

    working on continuous updated here. working on navigation route right now.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-function App() {
+import HomePage from "./pages/HomePage";
+//import ProductsPage from "./pages/ProductsPage";
+//import CartPage from "./pages/CartPage";
+//import OrdersPage from "./pages/OrdersPage";
+//import ExperimentsPage from "./pages/ExperimentsPage";
+//import PersonalizationPage from "./pages/PersonalizationPage";
+
+export default function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <header className="bg-blue-600 text-white p-4">
         <h1 className="text-xl font-bold">Mini E-Commerce Store</h1>
+        <h4 className="text-xl font-bold">Optimizely Inspired</h4>
       </header>
-
+      <Navbar />
       <main className="flex-1 p-6">
-        <p className="text-gray-700">Welcome to the store! Enjoy your stay.</p>
+        <Routes>
+           <Route path="/" element={<HomePage />} />
+          {/*<Route path="/products" element={<ProductsPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/orders" element={<OrdersPage />} />
+          <Route path="/experiments" element={<ExperimentsPage />} />
+          <Route path="/personalization" element={<PersonalizationPage />} /> */}
+        </Routes>
+        {/* <p className="text-gray-700">Welcome to the store! Enjoy your stay.</p> */}
       </main>
 
-      <footer className="bg-gray-200 text-center p-4">
+      <Footer />
+
+      {/* <footer className="bg-gray-200 text-center p-4">
         <p className="text-sm">© 2025 Mini E-Commerce Store</p>
-      </footer>
+      </footer> */}
     </div>
   );
 }
 
-export default App;
+//export default App;
